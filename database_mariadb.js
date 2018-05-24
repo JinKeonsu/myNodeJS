88d9f02841e232e2c4cb6c9d737e0a3bd4009d14
JinKeonsu <keonsu.jin@gmail.com>
2018-05-24 12:02:49 +0900

    modified

--- database_mariadb.js
+++ database_mariadb.js
@@ -1,8 +1,6 @@
 var nodeMaria = require('node-mariadb');
 require('dotenv').config();
 
-console.log(process.env.DB_PASS);
-
 //hs readable configuration.
 var connection = nodeMaria.createConnection({
   driverType: nodeMaria.DRIVER_TYPE_HANDLER_SOCKET,
@@ -20,22 +18,10 @@ connection.on('error', function(error){
 
 connection.on('connect', function(){
   console.log('db connected...');
-  connection.query('select * from mfuser;')
-    .on('result', function(res){
-      res.on('row', function(row){
-        console.log('result row -->' + row);
-      })
-    })
-    .on('error', function(err){
-      console.log('select error -->' + err);
-    })
-    .on('end', function(info){
-      console.log('result finished successfully');
+  connection.openIndex('memfee', 'mfuser', nodeMaria.HandlerSocket.PRIMARY, ['_uid', '_uname', '_uemail']
+  , function(err, hs){
+    hs.find([1], {limit:1},function(err, data){
+      console.log(data);
     });
-  // connection.openIndex('memfee', 'mfuser', nodeMaria.HandlerSocket.PRIMARY, ['_uid', '_uname', '_uemail']
-  // , function(err, hs){
-  //   hs.find([1], {limit:1},function(err, data){
-  //     console.log(data);
-  //   });
-  // });
+  });
 });
