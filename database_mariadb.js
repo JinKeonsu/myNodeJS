8f9598cca19939385d1d8e1f612bffe3de637b1f
JinKeonsu <keonsu.jin@gmail.com>
2018-05-24 11:29:25 +0900

    modified

--- database_mariadb.js
+++ database_mariadb.js
@@ -1,12 +1,12 @@
 var nodeMaria = require('node-mariadb');
-var config = require('dotenv').config();
+require('dotenv').config();
 
 //hs readable configuration.
 var connection = nodeMaria.createConnection({
   driverType: nodeMaria.DRIVER_TYPE_HANDLER_SOCKET,
-  host:config.env.DB_HOST,
+  host:process.env.DB_HOST,
   port:3306,
-  user:config.env.DB_USER,
-  password:config.env.DB_PASS,
-  database:config.env.DB_NAME
+  user:process.env.DB_USER,
+  password:process.env.DB_PASS,
+  database:process.env.DB_NAME
 });
